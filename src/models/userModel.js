323a6479d58368523b335a559b5b9a6a8e4066fe
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-16 19:18:04 -0400

    create the user model

---
+++ src/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
