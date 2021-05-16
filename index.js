a257e5d4a7b62ee03096e806e81913b546ba8f30
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-16 19:12:36 -0400

    finalize the setup for the project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
+mongoose.connect('mongodb://localhost:37017/CRMdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
