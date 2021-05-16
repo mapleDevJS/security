4b3aea1eafc819d61df82e2fb97910a826c9fe9b
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-16 19:39:57 -0400

    add the handlers for registration

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,3 +12,18 @@ export const loginRequired = (req, res, next) {
         return res.status(401).json({message: 'Unauthorized user!'});
     }
 }
+
+export const register = (req, res) => {
+    const newUser = new User(req.body);
+    newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+    newUser.save((err, user) => {
+        if (err) {
+            return res.status(400).send({
+                message: err
+            });
+        } else {
+            user.hashPassword = undefined;
+            return res.json(user);
+        }
+    });
+}
