8e17780958d0caf38a009ef6ed7a58fc79bb8488
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2021-05-16 21:58:07 -0400

    add JWT setup into index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import jsonwebtoken from 'jsonwebtoken';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
@@ -16,6 +17,20 @@ mongoose.connect('mongodb://localhost:37017/CRMdb', {
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// JWT setup
+app.use((req, res, next) => {
+    if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+        jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', (err, decode) => {
+            if (err) req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    } else {
+        req.user = undefined;
+        next();
+    }
+});
 
 routes(app);
 
